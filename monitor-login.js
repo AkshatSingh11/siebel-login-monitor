b236184db95a3bcc6743a5ae9b79cdbcab38171a
Akshat Singh <111632336+AkshatSingh11@users.noreply.github.com>
2026-04-08 14:09:24 +0530

    Update monitor-login.js

--- monitor-login.js
+++ monitor-login.js
@@ -14,7 +14,7 @@ const { chromium } = require('playwright');
 
   try {
     await page.goto(
-      'YOUR_LONG_URL_HERE',
+      'https://access.ex.indianoil.in/oam/server/obrareq.cgi?encquery%3DwuuojoyW8JsrnTFd6VYn%2F86v2WuU5YNS24cCYbV%2FEukwM%2F%2BpcrXRqswqGZuCm5ZLmYg9RizgjK1mZcTEZhh3ouLkN6D2%2Ft0CaGY4dGOHlNKLfQoslqp8oNOWRtEYFCXtNSb6ibktVMAmKP9LrrMOsYGzX85KLroHTuFB2MxXrAE8sKppgrWdCWC921BJN%2FBANy9QyWZFfzgqT6imyvVcD0PvqUQNlc5lc2IYP0N%2BvOWWbvlvUc6OkjgfG%2BkT0Js7kOPuy1IKCjv3Us8wYu5XfI4V5AVeve%2BGR3r0p4yT8jGSZaIwcaWDdUToyQgnJvVNrausz4jSUn72FAZ7964L%2BQ%3D%3D%20agentid%3DSIEBEL_IP24%20ver%3D1%20crmethod%3D2%26cksum%3D50d533f357b7c8942edd8314949c417ece6e08fa&ECID-Context=1.006JiuztxA8Bl3o5oVDCiY00CL8s001YT_%3BkXjE',
       {
         waitUntil: 'domcontentloaded',
         timeout: 60000
